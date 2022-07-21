6488be96b2702a948b7e071b6aa811cecd51ce9b
LoganKells <lkells@outlook.com>
2022-07-20 21:41:25 -0400

    Created Xcode workspace with ios-foundations-challenges.xcworkspace Initialized module-2-lesson-4-challenge project

---
+++ module-2-lesson-4-challenge/module-2-lesson-4-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  module-2-lesson-4-challenge
+//
+//  Created by Logan Kells on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ module-2-lesson-4-challenge/module-2-lesson-4-challenge/module_2_lesson_4_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  module_2_lesson_4_challengeApp.swift
+//  module-2-lesson-4-challenge
+//
+//  Created by Logan Kells on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct module_2_lesson_4_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
